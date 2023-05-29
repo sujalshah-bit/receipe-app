b9cc4025ec9fee656bfa50ccf2a38df62fae6b88
Sujal Shah <sujalshah28092004@gmail.com>
2023-05-29 12:06:06 +0530

    small bug fix

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors({
     origin: 'https://receipe-app-sigma.vercel.app',
-    credentials: true
+    
   }));
 app.use(cookieParser())
 app.use(express.json());
