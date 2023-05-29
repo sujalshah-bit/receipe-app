3019a0c8366f30db2673a755fab226035d5edb0e
Sujal Shah <sujalshah28092004@gmail.com>
2023-05-29 11:48:34 +0530

    small bug fix

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 
 // setup Middleware
 
-app.use('*',cors({
+app.use(cors({
     origin: 'https://receipe-app-sigma.vercel.app',
     credentials: true
   }));
